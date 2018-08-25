43c794453ad7dea947132a3e4ac72c877744fa2c
Deguchi <deguchi.git.hub@gmail.com>
2018-08-26 00:31:53 +0900

    Answer for subject (temp)

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,17 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    return calc_add_bottle(amount, 0)
+  end
+
+  # 追加される本数
+  def self.calc_add_bottle(add, remain)
+    add_new = (add + remain) / 3
+    remain_new = (add + remain) % 3
+
+    if 0 == add_new
+      return add
+    else
+      return add + calc_add_bottle(add_new, remain_new)
+    end
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  subject { BonusDrink.total_count_for(buy) }
+
+  context 'Buy 0' do
+    let(:buy) { 0 }
+    specify { is_expected.to eq 0 }
+  end
+
+  context 'Buy 1' do
+    let(:buy) { 1 }
+    specify { is_expected.to eq 1 }
+  end
+
+  context 'Buy 4' do
+    let(:buy) { 3 }
+    specify { is_expected.to eq 4 }
+  end
+
+  context 'Buy 11' do
+    let(:buy) { 11 }
+    specify { is_expected.to eq 16 }
+  end
+
+  context 'Buy 100' do
+    let(:buy) { 100 }
+    specify { is_expected.to eq 149 }
+  end
 end
